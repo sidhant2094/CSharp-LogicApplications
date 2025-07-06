62684e71291255270482d8ecd630f70993950007
Sidhant Malik <96983080+sidhant2094@users.noreply.github.com>
2025-07-06 17:03:07 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int saleAmount = 1900;
+int discount1 = 100;
+int discount2 = 50;
+
+Console.WriteLine($"Total Price: {saleAmount}");
+
+if (saleAmount > 1000)
+{
+    Console.WriteLine($"Discount Amount: {discount1}");
+    int finalPrice = saleAmount - discount1;
+    Console.WriteLine($"Final Price: {finalPrice}");
+
+}
+else if (saleAmount <= 1000)
+{
+    Console.WriteLine($"Discount Amount: {discount2}");
+    int finalPrice = saleAmount - discount2;
+    Console.WriteLine($"Final Price: {finalPrice}");    
+}
